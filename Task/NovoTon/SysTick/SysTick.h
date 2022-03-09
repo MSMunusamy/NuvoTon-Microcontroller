8e963b05f693d5ce5a62c5f18015125e2d0dd547
MSMunusamy <43171766+MSMunusamy@users.noreply.github.com>
2022-03-09 12:07:40 +0530

    Update SysTick.h

--- Task/NovoTon/SysTick/SysTick.h
+++ Task/NovoTon/SysTick/SysTick.h
@@ -1,3 +1,9 @@
+/**************************************************************************************
+ * NAME       : Munusamy M
+ * FILENAME   : Systick.h
+ * DESCRIPTION: Configure 1 Ms SysTick timer Interrupt
+ * DATE       : 16-02-2022
+***************************************************************************************/
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
 #include <stdio.h>
